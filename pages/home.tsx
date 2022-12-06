92cc5c62803d671baeeb07dbfb496a809adb7cee
Manuel Sebastian <seferinoubago12@gmail.com>
2022-12-05 22:31:58 -0600

    Borrado de otras rutas sin carpeta

--- pages/home.tsx
+++
@@ -1,3 +0,0 @@
-export default function home (){
-    return <h1> Este es la pagina de test </h1>
-}
--- pages/registration.tsx
+++
@@ -1,3 +0,0 @@
-export default function registration (){
-    return <h1> Este es la pagina de registro </h1>
-}
