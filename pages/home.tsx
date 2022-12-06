443e86c981c1877a4d8d181857d379ae6faf10fa
Manuel Sebastian <seferinoubago12@gmail.com>
2022-12-05 21:42:14 -0600

    posibles ritas realizadas y definidas

---
+++ pages/home.tsx
@@ -0,0 +1,3 @@
+export default function home (){
+    return <h1> Este es la pagina de test </h1>
+}
---
+++ pages/testpage/tests/test1.tsx
@@ -0,0 +1,3 @@
+export default function test1 (){
+    return <h1> Este es la pagina de test1 </h1>
+}
