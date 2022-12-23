5dbb87d2da6e3ec5c8c9cbc92594702976375c8a
Manuel Sebastian <seferinoubago12@gmail.com>
2022-12-22 20:37:00 -0600

    feature/create-prototype: create question

--- pages/testpage/tests/test1.tsx
+++ pages/testpage/tests/test1.tsx
@@ -1,3 +1,31 @@
-export default function test1 (){
-    return <h1> Este es la pagina de test1 </h1>
+import { useState } from "react"
+
+const test1 = () => { 
+    
+    const [resp, setResp] = useState('');
+    
+    return ( 
+    <>    
+        <form name="Formulario 1" action="" /* onSubmit={} */ className="formulario1">
+        
+        <div>
+        <h2> 1.- Para mí es importante recibir la aprobación de los demás. </h2>
+            <input type="radio" id="afirmacion" name="resp" value={1} onChange={(e) => setResp(e.target.value) } />
+            <label htmlFor="">Si</label>
+
+            <input type="radio" id="negacion" name="resp" value={0} onChange={(e) => setResp(e.target.value) } />
+            <label htmlFor="">No</label>
+        </div>
+
+        <div>
+        <h2> 2.- Odio equivocarme en algo.  </h2>
+            <input type="radio" id="afirmacion" name="resp" value={1} onChange={(e) => setResp(e.target.value) } />
+            <label htmlFor="">Si</label>
+
+            <input type="radio" id="negacion" name="resp" value={0} onChange={(e) => setResp(e.target.value) } />
+            <label htmlFor="">No</label>
+        </div>       
+        </form>
+    </>
+    )
 }
