d8a2a2638b29190cf0bc5953fb114f2c79bc1ef2
Manuel Sebastian <seferinoubago12@gmail.com>
2022-12-27 19:46:40 -0600

    feature/update_test: test basic functions developed, submit button, 2 standby functions

--- pages/test/test1.tsx
+++ pages/test/test1.tsx
@@ -1,39 +1,78 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 const Test1 = () => { 
     
     const [data, setData] = useState({
-        question1: ''
-    })
-    
+        dataQ1: '',
+        dataQ2: '',
+        dataQ3: ''
+    });
 
-    const handleChangeData = (e: React.ChangeEvent<any>) => {
+    // Convertir arrayStringdata a arratNumberdata 
+    // 
+
+    // Hacer sumatoria de arrayNumberdata
+    // 
+
+    const handleChangeData = (e: React.ChangeEvent<any>) =>{
         setData({
             ...data,
-            question1: e.target.value
+            [e.target.name]:e.target.value
         })
     }
 
-    console.log(data)
+    const handleSubmit = (e: React.ChangeEvent<any>) =>{
+
+        if(data){
+            e.preventDefault();
+            alert('el formulario se ha enviado')
+            console.log(data);
+            // console.log(stringArraydata);
+            // console.log(numberArraydata);
+            // console.log(datatotalvalue)
+        } else {
+            alert('Hay preguntas sin responder, complete el formulario')
+        }
+}
+
     return ( 
         <>    
-            <form name="Formulario 1" action=""  className="formulario1">
-                <div>
-                    <h2> 1.- Para mí es importante recibir la aprobación de los demás. </h2>
-                    <input type="radio" id="afirmacion" name="resp" value={1} onChange={handleChangeData} />
-                    <label htmlFor="">Si</label>
-                    <input type="radio" id="negacion" name="resp" value={0} onChange={handleChangeData} />
-                    <label htmlFor="">No</label>
-                </div>
+            <form onSubmit= {handleSubmit} name="Form1" action=""  className="Form1">
 
-                <div>
-                    <h2> 2.- Odio equivocarme en algo.  </h2>
-                    <input type="radio" id="afirmacion" name="resp" value={1} onChange={handleChangeData} />
-                    <label htmlFor="">Si</label>
-                    <input type="radio" id="negacion" name="resp" value={0} onChange={handleChangeData} />
-                    <label htmlFor="">No</label>
-                </div>       
-            </form>
+                <fieldset id="dataQ1">
+                    <legend> 1.- Para mí es importante recibir la aprobación de los demás. </legend>
+                    <label>
+                        <input type="radio" id="ansYes" name="dataQ1" value={1} onChange={handleChangeData} /> Si
+                    </label>
+                    <label >
+                        <input type="radio" id="ansNo" name="dataQ1" value={0} onChange={handleChangeData} /> No
+                    </label>
+                </fieldset>
+
+                <fieldset id="dataQ2">
+                    <legend>  2.- Odio equivocarme en algo. </legend>
+                    <label>
+                        <input type="radio" id="ansYes" name="dataQ2" value={1} onChange={handleChangeData} /> Si
+                    </label>
+                    <label >
+                        <input type="radio" id="ansNo" name="dataQ2" value={0} onChange={handleChangeData} /> No
+                    </label>
+                </fieldset>
+
+                <fieldset id="dataQ3">
+                    <legend> 3.- La gente que se equivoca, logra lo que se merece.  </legend>
+                    <label>
+                        <input type="radio" id="ansYes" name="dataQ3" value={1} onChange={handleChangeData} /> Si
+                    </label>
+                    <label >
+                        <input type="radio" id="ansNo" name="dataQ3" value={0} onChange={handleChangeData} /> No
+                    </label>
+                </fieldset>
+
+                <label htmlFor="">
+                    <input type="submit" name="" id="" />
+                </label>
+            </form>   
         </>
     )
 }
