025d9bf26cc3a2323ad5f0db086769addddb097c
Manuel Sebastian <seferinoubago12@gmail.com>
2023-01-02 21:04:01 -0600

    Feature/update_test: fixing problems sum

--- pages/test/test1.tsx
+++ pages/test/test1.tsx
@@ -1,18 +1,34 @@
 import { useState, useEffect } from "react";
-
 const Test1 = () => { 
     
+    // const [data, setData] = useState({
+    //     dataQ1: '',
+    //     dataQ2: '',
+    //     dataQ3: ''
+    // });
+
     const [data, setData] = useState({
-        dataQ1: '',
-        dataQ2: '',
-        dataQ3: ''
+        dataQ1: (Number),
+        dataQ2: (Number),
+        dataQ3: (Number)
     });
 
-    // Convertir arrayStringdata a arratNumberdata 
-    // 
+    const testvalues = Object.values(data);
 
-    // Hacer sumatoria de arrayNumberdata
-    // 
+    // const sumdata = testvalues.reduce((sumdata, dataQ1)=> sumdata + dataQ1, 0);
+    // const sumdata = ()=> {
+    //    const root =  testvalues.map(Math.sqrt)
+    // }
+        // testvalues.map)
+    const sumdata = testvalues.reduce<number>((acc, actualNum) => {
+        // console.log(acc)
+        // console.log(actualNum)
+        return actualNum 
+    }, 0)
+    
+//     const sumdata = (accumulator, curr) => accumulator + curr;
+// console.log(arr.reduce(reducer));
+
 
     const handleChangeData = (e: React.ChangeEvent<any>) =>{
         setData({
@@ -26,14 +42,12 @@ const Test1 = () => {
         if(data){
             e.preventDefault();
             alert('el formulario se ha enviado')
-            console.log(data);
-            // console.log(stringArraydata);
-            // console.log(numberArraydata);
-            // console.log(datatotalvalue)
+            // console.log(data);
+            console.log(sumdata);
         } else {
             alert('Hay preguntas sin responder, complete el formulario')
         }
-}
+    }
 
     return ( 
         <>    
